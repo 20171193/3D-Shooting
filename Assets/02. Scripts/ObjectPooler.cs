637957db8f1a74d3e1175d3a26ed860b6ba1170e
Shim Jae Chun <shimnaldo98@gmail.com>
2024-02-22 09:54:05 +0900

    Chore : fix ObjectPooler

--- Assets/02. Scripts/ObjectPooler.cs
+++ Assets/02. Scripts/ObjectPooler.cs
@@ -23,21 +23,38 @@ public class ObjectPooler : MonoBehaviour
             objectPool.Push(instance);
         }
     }
-    //public PooledObject GetPool(Vector3 position, Quaternion rotation)
-    //{
-    //    if (objectPool.Count>0)
-    //    {
-    //        PooledObject instance = objectPool.Pop();
-    //        instance.transform.position = position;
-    //        instance.transform.rotation = rotation;
-            
-    //    }
-    //}
+    public PooledObject GetPool(Vector3 position, Quaternion rotation)
+    {
+        if (objectPool.Count > 0)
+        {
+            PooledObject instance = objectPool.Pop();
+            instance.transform.position = position;
+            instance.transform.rotation = rotation;
+            instance.transform.parent = null;
+            instance.gameObject.SetActive(true);
+            return instance;
+        }
+        else
+        {
+            PooledObject instance = Instantiate(prefab);
+            instance.transform.position = position;
+            instance.transform.rotation = rotation;
+            instance.pooler = this;
+            return instance;
+        }
+    }
     public void ReturnPool(PooledObject instance)
     {
-        
-        // When returning to the pool, deactivate.
-        instance.gameObject.SetActive(false);
-        objectPool.Push(instance);
+        if (objectPool.Count < size)
+        {
+            // When returning to the pool, deactivate.
+            instance.gameObject.SetActive(false);
+            instance.transform.parent = transform;
+            objectPool.Push(instance);
+        }
+        else
+        {
+            Destroy(instance.gameObject);
+        }
     }
 }
